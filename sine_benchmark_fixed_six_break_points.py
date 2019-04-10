c0b18d0f37e1fcf92465d355272f2b3abe9828f3
Charles Jekel <cjekel@gmail.com>
2019-04-10 19:26:33 -0400

    add sp run, remove gpu runs

--- sine_benchmark_fixed_six_break_points.py
+++ sine_benchmark_fixed_six_break_points.py
@@ -2,14 +2,14 @@ import numpy as np
 import pwlf
 from time import time
 import os
+# force TF to use CPU
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
-
 breaks = np.array((0.0, 0.94, 2.96, 4.93, 7.02, 9.04, 10.0))
 
 n = np.logspace(3, 7, num=15, dtype=np.int)
 n_repeats = 10
-# run_times = np.zeros((3, n.size, n_repeats))
-run_times = np.load('bench_run_times/6_break_times.npy')
+run_times = np.zeros((2, n.size, n_repeats))
+
 for i, n_data in enumerate(n):
     # set random seed
     np.random.seed(256)
@@ -19,12 +19,18 @@ for i, n_data in enumerate(n):
     # add noise to the data
     y = np.random.normal(0, 0.05, size=n_data) + y
     for j in range(n_repeats):
-        # PWLF TF NO GPU fit
-        t4 = time()
+        # normal PWLF fit
+        t0 = time()
+        my_pwlf = pwlf.PiecewiseLinFit(x, y)
+        ssr = my_pwlf.fit_with_breaks(breaks)
+        t1 = time()
+        # PWLF TF fit
+        t2 = time()
         my_pwlf = pwlf.PiecewiseLinFitTF(x, y)
         ssr = my_pwlf.fit_with_breaks(breaks)
-        t5 = time()
-        run_times[2, i, j] = t5 - t4
+        t3 = time()
+        run_times[0, i, j] = t1 - t0
+        run_times[1, i, j] = t3 - t2
 
 np.save('bench_run_times/6_break_times.npy', run_times)
 np.save('bench_run_times/n.npy', n)
--- sine_benchmark_fixed_six_break_points_sp.py
+++ sine_benchmark_fixed_six_break_points_sp.py
@@ -2,30 +2,32 @@ import numpy as np
 import pwlf
 from time import time
 import os
+# force TF to use CPU
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
-breaks = np.array((0.0, 0.94, 2.96, 4.93, 7.02, 9.04, 10.0))
+breaks = np.array((0.0, 0.94, 2.96, 4.93, 7.02, 9.04, 10.0), dtype=np.float32)
 
 n = np.logspace(3, 7, num=15, dtype=np.int)
 n_repeats = 10
-run_times = np.zeros((3, n.size, n_repeats))
+run_times = np.zeros((2, n.size, n_repeats))
 
 for i, n_data in enumerate(n):
     # set random seed
     np.random.seed(256)
     # generate sin wave data
-    x = np.linspace(0, 10, num=n_data)
+    x = np.linspace(0, 10, num=n_data, dtype=np.float32)
     y = np.sin(x * np.pi / 2)
     # add noise to the data
-    y = np.random.normal(0, 0.05, size=n_data) + y
+    y = np.random.normal(0, 0.05, size=n_data).astype(np.float32) + y
     for j in range(n_repeats):
         # normal PWLF fit
         t0 = time()
-        my_pwlf = pwlf.PiecewiseLinFit(x, y)
+        my_pwlf = pwlf.PiecewiseLinFit(x, y, dtype='float32')
         ssr = my_pwlf.fit_with_breaks(breaks)
         t1 = time()
         # PWLF TF fit
         t2 = time()
-        my_pwlf = pwlf.PiecewiseLinFitTF(x, y)
+        my_pwlf = pwlf.PiecewiseLinFitTF(x, y, dtype='float32')
         ssr = my_pwlf.fit_with_breaks(breaks)
         t3 = time()
         run_times[0, i, j] = t1 - t0
