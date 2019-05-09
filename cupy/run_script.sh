85de4f398661905dec2cfd8d70dbc20d864c2987
cjekel <cjekel@gmail.com>
2019-05-09 13:01:53 -0400

    add new benchmark results

--- cupy/run_script.sh
+++ cupy/run_script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-python3 sine_benchmark_fixed_twenty_break_points_TFnoGPU.py
-python3 sine_benchmark_fixed_six_break_points_TFnoGPU.py
+python3 sine_benchmark_fixed_twenty_break_points.py
+python3 sine_benchmark_fixed_six_break_points.py
 python3 sine_benchmark_fixed_six_break_points_TFnoGPU.py
 python3 sine_benchmark_fixed_twenty_break_points_TFnoGPU.py
